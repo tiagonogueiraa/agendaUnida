bc8deccdfca0d9e8f1084772f500e7e767f7bff2
Tiago Nogueira <tiagonogueirajf@gmail.com>
2018-03-06 17:58:22 -0300

    Criado arquivo de pesquisar na tabela

---
+++ pesquisar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pesquisa</title>
+</head>
+<body>
+
+	<div class="container">
+
+		<form name="frmBusca" method="post" action="">
+
+		<input type="text" name="pesquisaNome">
+		<input type="submit" value="Buscar" name="">
+	
+		</form>
+
+		#http://rafaelcouto.com.br/sistema-de-busca-interna-com-php-mysql/
+	</div>
+</body>
+</html>
